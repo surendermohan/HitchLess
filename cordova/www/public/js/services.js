c5abc539fd792e4076bd15d2b66ed8362d633df9
Surender Mohan <smohan36@gmail.com>
2015-04-19 10:59:53 -0700

    clean up flight status

--- cordova/www/public/js/services.js
+++ cordova/www/public/js/services.js
@@ -1,5 +1,5 @@
 var module1 = angular.module('TodoService', ['ngResource']).factory('Todo', ['$resource', function ($resource) {
-    var Todo = $resource('http://localhost:3000/api/todo/:todoId', {}, {
+    var Todo = $resource('http://52.11.106.249:3000/api/todo/:todoId', {}, {
         update: { method: 'PUT'}
     });
     return Todo;
